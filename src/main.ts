dbdac9a9b1d8f64e902ad9efdf132c58a285894b
Vladimir V <vavanv.v@gmail.com>
2024-02-03 16:17:10 -0500

    add dev

--- src/main.ts
+++ src/main.ts
@@ -8,8 +8,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   //await app.listen(3000);
-  await app.listen(3000, () => {
-    console.log(`🚀 Application running at port 3000`);
+  await app.listen(process.env.APP_PORT, () => {
+    console.log(`🚀 Application running at port ${process.env.APP_PORT}`);
   });
 }
 bootstrap();
